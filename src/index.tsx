d135cd6242a8265702c85d5348ebf24606546bdd
skif-cezar <skif.cezar@gmail.com>
2022-09-19 14:52:17 +0300

    feat: add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
